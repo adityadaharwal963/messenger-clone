b5c1107b47fde36da23a7ee6327d0703ab794c1b
adityadaharwal963 <adityaprowork@gmail.com>
2024-03-07 23:26:30 +0530

    users & converstion routes added

--- app/components/SideBar/DesktopSidebar.tsx
+++ app/components/SideBar/DesktopSidebar.tsx
@@ -1,8 +1,36 @@
+'use client'
 
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
 
 const DesktopSidebar = () => {
+  const routes = useRoutes();
+  const [isOpen,setIsOpen] = useState(false);
+
   return (
-    <div>
+    <div className="
+    hidden
+    lg:fixed
+    lg:inset-y-0
+    lg:left-0
+    lg:z-40
+    lg:w-20
+    lg:px-6
+    lg:overflow-y-auto
+    lg:bg-white
+    lg:border-r-[1px]
+    lg:pb-4
+    lg:flex
+    lg:flex-col
+    justify-between
+     ">
+      <nav className="
+      mt-4
+      flex
+      flex-col
+      justify-between">
+        <ul></ul>
+      </nav>
       desktop sidebar!
     </div>
   )
--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -1,3 +1,37 @@
 import { useMemo } from "react";
-import { useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import {HiChat} from "react-icons/hi";
+import { HiArrowLeftOnRectangle , HiUsers } from "react-icons/hi2";
+import { signOut } from "next-auth/react";
+import useConversation from "./useConversation";
+
+const useRoutes = () => {
+    const pathname = usePathname();
+    const {conversationId} = useConversation();
+
+    const routes = useMemo(() => [
+        {
+            label : 'Chat',
+            href : '/converstions',
+            icon : HiChat,
+            active : pathname == '/converstion' || !!conversationId
+
+        },
+        {
+            label: 'Users',
+            href : '/users',
+            icon : HiUsers,
+            active : pathname == '/users'
+        },
+        {
+            label : 'Logout',
+            href : '#',
+            onClick : () => signOut(),
+            icon : HiArrowLeftOnRectangle
+        }
+    ],[pathname,conversationId]);
+
+    return routes;
+}
+
+export default useRoutes;
